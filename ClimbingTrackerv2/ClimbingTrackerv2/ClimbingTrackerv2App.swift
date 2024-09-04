a17040aef482569c139263974f45e0f52339b457
Raymond-tan Tran <raymondtran2007@gmail.com>
2024-09-03 20:19:59 -0700

    Initial commit

---
+++ ClimbingTrackerv2/ClimbingTrackerv2/ClimbingTrackerv2App.swift
@@ -0,0 +1,17 @@
+//
+//  ClimbingTrackerv2App.swift
+//  ClimbingTrackerv2
+//
+//  Created by Raymond-tan Tran on 9/3/24.
+//
+
+import SwiftUI
+
+@main
+struct ClimbingTrackerv2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
